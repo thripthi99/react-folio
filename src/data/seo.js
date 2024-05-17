182e10bbd5b9212a02d317e6fbacba2bf9ea0fdc
thripthi99 <thripthikolthige@gmail.com>
2024-05-17 17:53:00 +0530

    Added files

---
+++ src/data/seo.js
@@ -0,0 +1,38 @@
+const SEO = [
+	{
+		page: "home",
+		description:
+			"I am a frontend developer with expertise in React.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies.",
+		keywords: ["Thripthi", "Thripthi K M", "Thripthi Gowda"],
+	},
+
+	{
+		page: "about",
+		description:
+			"I am a frontend developer with expertise in React.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies.",
+		keywords: ["Thripthi", "Thripthi K M", "Thripthi Gowda"],
+	},
+
+	{
+		page: "articles",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+		keywords: ["Thripthi", "Thripthi K M", "Thripthi Gowda"],
+	},
+
+	{
+		page: "projects",
+		description:
+			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to.",
+		keywords: ["Thripthi", "Thripthi K M", "Thripthi Gowda"],
+	},
+
+	{
+		page: "contact",
+		description:
+			"If you're interested in collaborating on a project, feel free to reach out to me. I'm always open to new ideas and opportunities.",
+		keywords: ["Thripthi", "Thripthi K M", "Thripthi Gowda"],
+	},
+];
+
+export default SEO;
